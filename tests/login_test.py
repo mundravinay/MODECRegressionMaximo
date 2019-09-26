8508822edc8cda49204ea6721cccbc189486ac45
vinay mundra <vinay_mundra@yahoo.com>
2019-09-26 16:17:04 +0100

    adding demo file

--- tests/login_test.py
+++ tests/login_test.py
@@ -1,10 +1,9 @@
 import allure
-import moment
-from selenium import webdriver
 import pytest
 from Pages.loginPage import LoginPage
 from Pages.homePage import HomePage
 from utils import utils as utils
+import moment
 
 
 @pytest.mark.usefixtures("test_setup")
@@ -19,11 +18,13 @@ class TestLogin():
         login.click_login()
 
     def test_logout(self):
+        global driver
         try:
             driver = self.driver
             homepage = HomePage(driver)
             homepage.click_logout()
             x = driver.title
+            print(x)
             assert x == "Start Center"
             # Start Center
 
@@ -32,21 +33,21 @@ class TestLogin():
             print(error)
             currTime = moment.now().strftime("%d-%m-%Y_%H-%M-%S")
             testName = utils.whoami()
-            screenshotName = testName + "_" + currTime
-            allure.attach(self.driver.get_screenshot_as_png(), name=screenshotName,
+            screenshotsName = testName + "_" + currTime
+            allure.attach(self.driver.get_screenshot_as_png(), name=screenshotsName,
                           attachment_type=allure.attachment_type.PNG)
             driver.get_screenshot_as_file(
-                "C:/Users/Vinay/PycharmProjects/MODECRegressionMaximo/screenshots/" + screenshotName + ".png")
+                "C:/Users/Vinay/PycharmProjects/MODECRegressionMaximo/screenshots/" + screenshotsName + ".png")
             raise
         except:
             print("There was an exception")
             currTime = moment.now().strftime("%d-%m-%Y_%H-%M-%S")
             testName = utils.whoami()
-            screenshotName = testName + "_" + currTime
-            allure.attach(self.driver.get_screenshot_as_png(), name=screenshotName,
+            screenshotsName = testName + "_" + currTime
+            allure.attach(self.driver.get_screenshot_as_png(), name=screenshotsName,
                           attachment_type=allure.attachment_type.PNG)
             raise
         else:
             print("No Exceptions occurred")
         finally:
-            print("Login succesfull")
+            print("Login successful")
